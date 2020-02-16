1d9a6fecfe1cc4a2751d11992626c6e6ada4e9e7
KornevSergei <4Kornev@gmail.com>
2020-02-16 07:59:45 +0300

    Step 3

--- app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
+++ app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
@@ -117,7 +117,7 @@ public class CreateOrderActivity extends AppCompatActivity {
         //создаём строку с добавками
         String additions;
         if (builderAdditions.length() > 0)
-            additions = getString(R.string.need_additions) + builderAdditions.toString();
+            additions = "\n" + getString(R.string.need_additions) + builderAdditions.toString();
         else {
             additions = "";
         }
--- app/src/main/java/com/example/cafeorder/OrderDetailActivity.java
+++ app/src/main/java/com/example/cafeorder/OrderDetailActivity.java
@@ -2,13 +2,31 @@ package com.example.cafeorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderDetailActivity extends AppCompatActivity {
+
+    //переменная для установки текста
+    private TextView textViewOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+
+        //связываем
+        textViewOrder = findViewById(R.id.textViewOrder);
+
+        //присваиваем занчение,проверяем на ключ  и если не ок - отправляем в логин активити
+        Intent intent = getIntent();
+        if (intent.hasExtra("order")) {
+            String order = intent.getStringExtra("order");
+            textViewOrder.setText(order);
+        } else {
+            Intent backToLogin = new Intent(this, LoginActivity.class);
+            startActivity(backToLogin);
+        }
     }
 }
